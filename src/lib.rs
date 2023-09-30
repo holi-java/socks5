3dbe4697b8e4a4193eee612eb986e098bfdec280
feexon <feexon@foxmail.com>
2023-09-30 21:45:28 +0800

    Added: `Stream` trait to make code test simply

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 use std::net::SocketAddr;
 
 use tokio::{
-    io::{copy_bidirectional, AsyncReadExt, AsyncWriteExt},
+    io::{copy_bidirectional, AsyncRead, AsyncReadExt, AsyncWrite, AsyncWriteExt},
     net::{TcpListener, TcpStream},
 };
 
@@ -15,35 +15,40 @@ pub const RSV: u8 = 0x0;
 pub const IPV4: u8 = 0x1;
 pub const UNSPECIFIED_SOCKET_ADDR: [u8; 6] = [0x0, 0x0, 0x0, 0x0, 0x0, 0x0];
 
+/// Just used as typealias only for bounded generic parameters.
+trait Stream: AsyncRead + AsyncWrite + Unpin {}
+
+impl<T: ?Sized> Stream for T where T: AsyncRead + AsyncWrite + Unpin {}
+
 pub async fn start(port: u16) -> IOResult<()> {
     let server = TcpListener::bind(format!("127.0.0.1:{port}")).await?;
     loop {
-        tokio::spawn(run(server.accept().await?));
+        tokio::spawn(run(server.accept().await?.0));
     }
 }
 
-async fn run((mut downstream, _): (TcpStream, SocketAddr)) -> IOResult<()> {
+async fn run<S: Stream>(mut client: S) -> IOResult<()> {
     // Negotiation
-    let _version = downstream.read_u8().await?;
-    let _nmethods = downstream.read_u8().await? as usize;
-    let _methods = downstream
+    let _version = client.read_u8().await?;
+    let _nmethods = client.read_u8().await? as usize;
+    let _methods = client
         .read_exact(vec![0u8; _nmethods].as_mut_slice())
         .await?;
-    downstream.write_all(&[VER, OK]).await?;
+    client.write_all(&[VER, OK]).await?;
 
     // Connect
-    let _version = downstream.read_u8().await?;
-    let _cmd = downstream.read_u8().await?;
-    let _rsv = downstream.read_u8().await?;
-    let _atype = downstream.read_u8().await?;
-    let ip = downstream.read_u32().await?;
-    let port = downstream.read_u16().await?;
+    let _version = client.read_u8().await?;
+    let _cmd = client.read_u8().await?;
+    let _rsv = client.read_u8().await?;
+    let _atype = client.read_u8().await?;
+    let ip = client.read_u32().await?;
+    let port = client.read_u16().await?;
     let addr = SocketAddr::from((ip.to_be_bytes(), port));
-    downstream.write_all(&[VER, OK, RSV, IPV4]).await?;
-    downstream.write_all(&UNSPECIFIED_SOCKET_ADDR).await?;
+    client.write_all(&[VER, OK, RSV, IPV4]).await?;
+    client.write_all(&UNSPECIFIED_SOCKET_ADDR).await?;
 
     // Send
     let mut upstream = TcpStream::connect(addr).await?;
-    _ = copy_bidirectional(&mut upstream, &mut downstream).await?;
+    _ = copy_bidirectional(&mut upstream, &mut client).await?;
     Ok(())
 }
