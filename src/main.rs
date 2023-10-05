297d193e787f7d540fa8374aecee504fc4a46644
feexon <feexon@foxmail.com>
2023-10-05 21:08:10 +0800

    parse port argument

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
+use std::env::args;
+
 const DEFAULT_SOCKS5_PORT: u16 = 1080;
 
 #[tokio::main]
 async fn main() {
-    socks5::start(DEFAULT_SOCKS5_PORT).await.unwrap();
+    let port = args()
+        .nth(1)
+        .map(|it| it.parse::<u16>())
+        .unwrap_or(Ok(DEFAULT_SOCKS5_PORT));
+    socks5::start(port.unwrap()).await.unwrap();
 }
